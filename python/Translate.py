1f8d64df8e5407c54b28d6c5b9f0678b715a4da2
Greg Jones <greg@rockmelt.com>
2012-03-07 13:47:19 -0800

    rule writing framework
    Change-Id: I987a981a0d8a63de2a00ea5570e2a57462c6f252

--- python/Translate.py
+++ python/Translate.py
@@ -7,6 +7,11 @@ class Word:
 		self.spanish = spanish
 
 class Translator:
+	
+	def __init__(self):
+		self.rules = []
+		self.addRules()
+	
 
 	def readInDictionary(self, file):
 		infile = open(file, 'rb')
@@ -24,17 +29,42 @@ class Translator:
 		data = []
 		for line in lines:
 			sentence = []
-			lineSpl = line.split()
+			lineSpl = map(lambda word: word.strip('.,:"').lower(), line.split())
 			for w in lineSpl:
 				word = Word(w, self.dict.get(w, 'NOTFOUND'))
-				print word.spanish + "   " + word.english
 				sentence.append(word)
 			data.append(sentence)
 		return data
+		
+	def addRules(self):
+		
+		def mergeTo(sentence):
+			newSentence = []
+			sentenceLen = len(sentence)
+			for idx,word in enumerate(sentence):
+				if word.english == 'to' and idx < sentenceLen - 1 and sentence[idx+1].english.startswith('to'):
+					continue
+				newSentence.append(word)
+			return newSentence
+		self.rules.append(mergeTo)
+		
+	def applyRule(self, rule, sentence):
+		return rule(sentence)
 
 	# TODO
 	def translate(self, data):
-		return ''
+		result = ""
+		for sentence in data:
+			
+			for rule in self.rules:
+				sentence = self.applyRule(rule, sentence)
+
+			for idx,word in enumerate(sentence):
+				if idx == 0:
+					word.english = word.english[0].title() + word.english[1:]
+				result += word.english + " "
+			result = result[:-1] + ". \n\n"
+		return result
 
 if __name__ == '__main__':
 	gatoFile = '../data/gato.txt'
@@ -43,3 +73,4 @@ if __name__ == '__main__':
 	translator.readInDictionary(dictFile)
 	data = translator.readInData(gatoFile)
 	translated = translator.translate(data)
+	print translated
